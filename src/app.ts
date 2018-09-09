af96cf995344ac4e97ad153464b156bcd3033fe5
Corinne Roosen <corinnesmailbox@gmail.com>
2018-09-09 18:31:12 +0200

    Add mining method

--- src/app.ts
+++ src/app.ts
@@ -11,7 +11,7 @@ export class Block {
 
     constructor(index: number, nonce: number, data: string, prevHash: string) {
         this.index = index;
-        this.nonce = 0;
+        this.nonce = nonce;
         this.timestamp = new Date();
         this.data = data;
         this.prevHash = prevHash;
@@ -19,7 +19,9 @@ export class Block {
     }
 
     public hashBlock(): string {
-        console.log("hash block");
+
+        console.log("Hashing block");
+
         const index = this.index.toString();
         const nonce = this.nonce.toString();
         const timestamp = this.timestamp.toString();
@@ -44,7 +46,9 @@ export class BlockChain {
     }
 
     public addBlock(block: Block): Block {
-        console.log("add block");
+
+        console.log("Adding block");
+
         // gets block with Data
         // add index and prevHash to the Block
         const lastBlock = this.chain[this.chain.length - 1];
@@ -65,32 +69,42 @@ export class BlockChain {
 
     // QUESTION how to improve performance?
     public mineBlock(block: Block): Block {
-        console.log("mine block");
+
+        console.log("Mining block");
+
         // take content of block
-        // check if hash complies to difficulty
+        // check if hash complies to difficulty number
         // if not, nonce++ and generate new hash, check again
         // else, generate the new hash
         // TODO "00" is now hardcoded
+        // TODO this needs to be a nicer recursive function
         while (block.hash.substring(0, this.difficulty) !== "00") {
 
-            console.log(`mining block ${block.index}: nonce: ${block.nonce}`);
+            console.log(`Mining block ${block.index}: nonce: ${block.nonce} hash: ${block.hash}`);
 
             block.nonce++;
-            block.hashBlock();
+            block.hash = block.hashBlock();
         }
+
+        console.log(`Block ${block.index} mined with nonce ${block.nonce} and hash ${block.hash}`);
 
         return block;
     }
 }
 
 // create new blockchain with difficulty of 2
+// TODO difficulty is hardcoded to 00
 const chainy = new BlockChain(2);
 
 // create new block
-const block01 = new Block(0,0,"New Block!","");
+const block1 = new Block(0,0,"First new block with some data","");
+const block2 = new Block(0,0,"This is another block","");
+const block3 = new Block(0,0,"Yet another new block!","");
 
 // add to chain
-chainy.addBlock(block01);
+chainy.addBlock(block1);
+chainy.addBlock(block2);
+chainy.addBlock(block3);
 
 // show output of chain
-// console.log(JSON.stringify(chainy, null, 4));
+console.log(JSON.stringify(chainy, null, 4));

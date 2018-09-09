5b03e3e22c5d87800de64112c3db01620f1253c2
Corinne Roosen <corinnesmailbox@gmail.com>
2018-09-09 16:41:07 +0200

    Update blockchain code

--- src/app.ts
+++ src/app.ts
@@ -7,68 +7,90 @@ export class Block {
     public timestamp: Date;
     public data: string;
     public prevHash: string;
-    public hash: string; // TODO check if this should be here + check constructor
+    public hash: string;
 
     constructor(index: number, nonce: number, data: string, prevHash: string) {
         this.index = index;
-        this.nonce = nonce;
+        this.nonce = 0;
         this.timestamp = new Date();
         this.data = data;
         this.prevHash = prevHash;
         this.hash = this.hashBlock();
     }
 
-    // hash whole block contents: serialize then stringify to create a new hash for this block
     public hashBlock(): string {
-        return SHA256(this.index + this.nonce + this.timestamp + this.data + this.prevHash).toString();
+        console.log("hash block");
+        const index = this.index.toString();
+        const nonce = this.nonce.toString();
+        const timestamp = this.timestamp.toString();
+
+        return sha256(index + nonce + timestamp + this.data + this.prevHash);
     }
 
-    public mineBlock(difficulty): string {
-      // mine
+    public getHashBlock(): string {
+        return this.hash;
     }
 }
 
 // class BlockChain
 export class BlockChain {
-
-    // amount of zero's
-    public static difficulty = 2;
-
-    // blocks in the chain
-    public blocks: Array<Block>;
+    private chain: Array<Block>;
+    public difficulty: number;
 
     // QUESTION how do we store a blockchain in a node? Memory? Database?
-    constructor() {
-        this.blockchain = Array(this.createFirstBlock());
+    constructor(md: number) {
+        this.chain = new Array<Block>(new Block(0,0,"First!",""));
+        this.difficulty = md;
     }
 
-    // create the first block in the chain
-    createFirstBlock() {
-        return new Block(0, 0, "first one!", "" );
+    public addBlock(block: Block): Block {
+        console.log("add block");
+        // gets block with Data
+        // add index and prevHash to the Block
+        const lastBlock = this.chain[this.chain.length - 1];
+        block.index = lastBlock.index + 1;
+        block.prevHash = lastBlock.hash;
+
+        // mine block; generate hash with nonce
+        // QUESTION mined == Pow?
+        // TODO put in Promise?
+        this.mineBlock(block);
+
+        // push mined block to chain
+        this.chain.push(block);
+
+        // ???
+        return block;
     }
 
-    // get latest block in the chain
-    // FIXME improve this, see code assignment
-    getLastBlock() {
-        return this.blockchain[this.blockchain.length - 1];
-    }
+    // QUESTION how to improve performance?
+    public mineBlock(block: Block): Block {
+        console.log("mine block");
+        // take content of block
+        // check if hash complies to difficulty
+        // if not, nonce++ and generate new hash, check again
+        // else, generate the new hash
+        // TODO "00" is now hardcoded
+        while (block.hash.substring(0, this.difficulty) !== "00") {
 
-    addBlock(block) {
-        block.prevHash = this.getLatestBlock.hash;
-        block.hash = block.hashBlock();
-        this.blockchain.push(block);
-    }
+            console.log(`mining block ${block.index}: nonce: ${block.nonce}`);
 
-    // mine block
-    mineBlock(data: string): Block {
-        const lastBlock = this.getLastBlock();
-        const newBlock = new Block(this.getLastBlock.index + 1, 0, data, this.getLastBlock.hash )
-
-        while(true) {
-            // mining here
+            block.nonce++;
+            block.hashBlock();
         }
-    }
 
+        return block;
+    }
 }
 
-// generate blocks
+// create new blockchain with difficulty of 2
+const chainy = new BlockChain(2);
+
+// create new block
+const block01 = new Block(0,0,"New Block!","");
+
+// add to chain
+chainy.addBlock(block01);
+
+// show output of chain
+// console.log(JSON.stringify(chainy, null, 4));
